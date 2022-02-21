2c66d35352b214a410404c7982217c269084023e
UmeshDSV <dumesh.sai@gmail.com>
2022-02-21 12:53:27 +0530

    removed mongo uri

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const uri = process.env.MONGO_DB_URI || "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true"
+const uri = process.env.MONGO_DB_URI
 mongoose.connect(uri, {
     useNewUrlParser: true
 })
